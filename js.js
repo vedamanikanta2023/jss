87e6e74e6bc391bc61a011871fbc08d9a13e785f
vedamanikanta <manikanta.vedam@gmail.com>
2025-09-28 23:58:27 +0530

    finding wth Math.max

--- js.js
+++ js.js
@@ -1,13 +1,6 @@
 function findLargest(arr) {
-    //Suppose first element is the largest
-    let largest = arr[0]; 
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > largest) {
-            // Update the largest if a bigger element is found
-            largest = arr[i]; 
-        }
-    }
-    return largest;
+    // Math.max() is used to find the largest number
+    return Math.max(...arr); 
 }
 
 console.log(findLargest([99, 5, 3, 100, 1]));
