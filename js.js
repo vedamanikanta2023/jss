8583a727f50bc5682f2889c4074e55016b20d321
vedamanikanta <manikanta.vedam@gmail.com>
2025-09-29 18:54:15 +0530

    fibonakki seriies

--- js.js
+++ js.js
@@ -1,12 +1,14 @@
-function isPrime(num) {
-    if (num <= 1) 
-        return false;
-    for (let i = 2; i < num; i++) 
-    {
-        if (num % i === 0) 
-            return false;
+function fibonacci(n) {
+    let num1 = 0, num2 = 1, nextNum;
+
+    console.log("Fibonacci Sequence:");
+
+    for (let i = 1; i <= n; i++) {
+        console.log(num1);
+        nextNum = num1 + num2;
+        num1 = num2;
+        num2 = nextNum;
     }
-    return true;
 }
 
-console.log(isPrime(7));
+fibonacci(7);
