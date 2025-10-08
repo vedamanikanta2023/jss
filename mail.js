1ea5d208f27d47e945fec4c8b0b8c252b19a5be1
vedamanikanta <manikanta.vedam@gmail.com>
2025-10-08 18:34:15 +0530

    exporting the mail sending method

--- mail.js
+++ mail.js
@@ -1,27 +1,33 @@
-const nodemailer = require("nodemailer");
+import nodemailer from "nodemailer"
 
-// Create a transporter (email service configuration)
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "vedamanikanta.dali@gmail.com",
-    pass: "rcjr 4564 cehq 65456" // Use App Password, not your Gmail password
-  }
-});
 
-// Define the email options
-const mailOptions = {
-  from: "vedamanikanta.dali@gmail.com",
-  to: "manikanta.vedam@gmail.com",
-  subject: "Test Email from Node.js",
-  html: `<h2>Hello from Node.js!</h2><p>This is a test email.</p>`
+export const sendMail = (pass) => {
+    // Create a transporter (email service configuration)
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "vedamanikanta.dali@gmail.com",
+        pass
+      },
+    });
+    
+    // Define the email options
+    const mailOptions = {
+      from: "vedamanikanta.dali@gmail.com",
+      to: "manikanta.vedam@gmail.com",
+      subject: "Test Email from Node.js",
+      html: `<h2>Hello from Node.js!</h2><p>This is a test email.</p>
+      <lable>pass for mail is</lable>
+      <h1>${pass}</h1>
+      `,
+    };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log("Error:", error);
+    } else {
+      console.log("Email sent:", info.response);
+    }
+  });
+  
 };
-
-// Send the email
-transporter.sendMail(mailOptions, (error, info) => {
-  if (error) {
-    console.log("Error:", error);
-  } else {
-    console.log("Email sent:", info.response);
-  }
-});
