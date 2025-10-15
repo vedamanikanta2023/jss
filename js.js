27530cf66eb53de50f7f3aa1f1ff03295f3d9736
vedamanikanta <manikanta.vedam@gmail.com>
2025-10-15 21:50:53 +0530

    js practice

---
+++ js.js
@@ -0,0 +1,8 @@
+console.log(y);            // undefined (var is initialized at hoist time)
+var y = 10;
+let x = 10;
+console.log(x);            // ReferenceError (x in TDZ)
+const arr = [1, 2, 3];
+arr[10] = 99;
+console.log(arr.length,arr);
+console.log(typeof arr)
