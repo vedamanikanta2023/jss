a10dbdb70e10d682c7356548644061f60475ff34
vedamanikanta <manikanta.vedam@gmail.com>
2025-10-22 21:42:18 +0530

    chekcing typeof null

--- js.js
+++ js.js
@@ -6,4 +6,5 @@ const arr = [1, 2, 3];
 arr[10] = 99;
 console.log(arr.length,arr);
 console.log(typeof arr)
-console.log( typeof undefined)
+console.log( typeof undefined);
+console.log(typeof null)
