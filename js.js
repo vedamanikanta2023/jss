6064543f66871fa967b7d1bc84c589980c493d24
vedamanikanta <manikanta.vedam@gmail.com>
2025-10-24 23:08:15 +0530

    most consecutive letters array

--- js.js
+++ js.js
@@ -1,16 +1,39 @@
-console.log(a); // undefined
-var a = 10;
-let b = 4092;
-sayHi(); // Works due to hoisting
-function sayHi() { console.log("Hi"); }
+const lettersCount = {};
+let letter;
+let count = 1;
 
- function displayInfo(name, age, ...otherInfo) {
-        console.log(`Name: ${name}, Age: ${age}`);
-        console.log(`Other Info: ${otherInfo}`); // otherInfo will be an array
+function maxConsecutiveLetters(str) {
+  const pushToCount = (letter2) => {
+    if (Object.keys(lettersCount).includes(letter2)) {
+      lettersCount[letter2] =
+        count > lettersCount[letter2] ? count : lettersCount[letter2];
+    } else {
+      lettersCount[letter2] = count;
     }
-    displayInfo('John', 30, 'Developer', 'NYC');
+    count = 1;
+  };
 
-      const [first, second, ...remaining] = [1, 2, 3, 4, 5];
-    console.log(first);    // 1
-    console.log(second);   // 2
-    console.log(remaining); // [3, 4, 5]
+  for (let i = 0; i < str.length - 1; i++) {
+    const letter2 = str[i];
+    if (letter2 === str[i + 1]) {
+      count = count + 1;
+    } else {
+      pushToCount(letter2)
+      count = 1;
+    }
+  }
+  pushToCount(str[str.length-1]);
+}
+
+maxConsecutiveLetters("aaabbcdddggg");
+
+const arrCount = Object.values(lettersCount);
+const biggest = Math.max(...arrCount);
+const resultantArr = [];
+
+for (let i of Object.keys(lettersCount)) {
+  if (lettersCount[i] === biggest) {
+    resultantArr.push({ [i]: biggest });
+  }
+}
+console.log({resultantArr});
