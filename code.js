43e4cb9c12307798b9e70da0f50863d0da10ef72
vedamanikanta <manikanta.vedam@gmail.com>
2025-11-15 19:10:37 +0530

    handling asynchronus errors

--- code.js
+++ code.js
@@ -1,25 +1,44 @@
-
-const studentsList = [{
-    rollNo: 1111,
-    name: 'name1'
-}, {
-    rollNo: 2222,
-    name: 'name2'
-}]
-
-interface StudenType{
-
-}
-
-const [student, setStudent] = useState<StudenType>(studentsList);
-
-const updateStudentName = ()=>{
-  const updatedStudents = studentsList.map((student)=>{
-    if (student.rollNo===2222){
-      student.name = "name3";
-      return student
+function someAsynchronousFunction(isResolve = true) {
+  console.log("time", new Date().toLocaleTimeString());
+  return new Promise((resolve, reject) => {
+    if (isResolve) {
+      setTimeout(() => {
+        resolve({ name: "vedam", age: 28 });
+      }, 1000*3);
+    } else {
+      reject("error in Promise else");
     }
-    return student
-  })
-  setStudent(updatedStudents);
+  });
 }
+
+someAsynchronousFunction(false)
+  .then((data) => {
+    console.log("in then time", new Date().toLocaleTimeString());
+    console.log("Data received:", data);
+  })
+  .catch((error) => {
+    console.log("in catch time", new Date().toLocaleTimeString());
+    console.error("An error occurred asynchronously (Promise):", error.message);
+  });
+
+async function fetchData() {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    console.log("Data 8790965: ", response);
+    if (!response.ok) {
+      throw new Error(`HTTP erro! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log("Data received: ", data);
+  } catch (e) {
+    console.error("An error occured asynchronously (async/await):", e.message);
+  }
+}
+
+// fetchData();
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
