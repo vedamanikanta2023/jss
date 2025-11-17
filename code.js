0db376d8a38bb07e89b48adb532318da383e327b
vedamanikanta <manikanta.vedam@gmail.com>
2025-11-17 23:02:02 +0530

    event loop

--- code.js
+++ code.js
@@ -1,7 +1,17 @@
-const name = "manikanta"
-try {
-  console.log(name);
-  console.log(name2);
-} catch (error) {
-  console.error(`in catch block `,error);
-}
+// const name = "manikanta"
+// try {
+//   console.log(name);
+//   console.log(name2);
+// } catch (error) {
+//   console.error(`in catch block `,error);
+// }
+
+console.log("This is the first statement");
+
+setTimeout(function() {
+    console.log("This is the second statement");
+}, 1000);
+
+Promise.resolve(console.log("hi I'm promise"))
+
+console.log("This is the third statement");
