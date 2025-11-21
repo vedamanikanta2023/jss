9f64ba2f22382c50df8589d7daae182f2f270216
vedamanikanta <manikanta.vedam@gmail.com>
2025-11-21 16:37:42 +0530

    map in js

--- code.js
+++ code.js
@@ -1,13 +1,13 @@
-const userDetails = {
-  name: "vedamanikanta",
-  age: 26,
-  bike: {
-    bikeName: "bik2340",
-    cc: 140,
-  },
-};
+const check = ()=>{
+  throw new Error("Param needed ra yedava");
+}
+const functionRegistry = new Map();
+function mySpecialFunction(name= check()) {
+  console.log("This is a special function.", name);
+}
+functionRegistry.set("uniqueID123", mySpecialFunction);
+functionRegistry.set("uniqueID122", ()=>{});
 
-const copyUserDetails = JSON.parse(JSON.stringify(userDetails));
-copyUserDetails.bike.bikeName = "pulsar";
-console.log({ userDetails, copyUserDetails });
-
+// Retrieve and call the function using your custom ID
+functionRegistry.get("uniqueID123")("chekar");
+console.log(  functionRegistry)
