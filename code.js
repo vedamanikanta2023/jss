514c4fa3aba8f354162db230d7815548d38a2a8e
vedamanikanta <manikanta.vedam@gmail.com>
2025-11-25 00:12:18 +0530

    closure

--- code.js
+++ code.js
@@ -1,8 +1,20 @@
-function show(){
-  {
-    var x = 10;
-    var y = 123;
+const not = "hai";
+function outer(){
+  let count = 0
+  return function inner(){
+    count ++;
+    console.log(count)
+    return count;
   }
-  console.log(x,y)
 }
-show()
+
+
+const f1 = outer();
+f1()
+f1()
+const f2 = outer();
+f2()
+f2()
+f2()
+f2()
+console.log(f1,f2)
