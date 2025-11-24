df4f40f80005cb151cae4d5f9a4f5eea936a9872
vedamanikanta <manikanta.vedam@gmail.com>
2025-11-24 23:07:43 +0530

    var practice

--- code.js
+++ code.js
@@ -1,13 +1,8 @@
-const check = ()=>{
-  throw new Error("Param needed ra yedava");
+function show(){
+  {
+    var x = 10;
+    var y = 123;
+  }
+  console.log(x,y)
 }
-const functionRegistry = new Map();
-function mySpecialFunction(name= check()) {
-  console.log("This is a special function.", name);
-}
-functionRegistry.set("uniqueID123", mySpecialFunction);
-functionRegistry.set("uniqueID122", ()=>{});
-
-// Retrieve and call the function using your custom ID
-functionRegistry.get("uniqueID123")("chekar");
-console.log(  functionRegistry)
+show()
