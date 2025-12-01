ed5227756324fb3813d60f7068e52c0f1dda1a72
vedamanikanta <manikanta.vedam@gmail.com>
2025-12-01 23:56:06 +0530

    ltimindtree questions

--- code.js
+++ code.js
@@ -1,10 +1,19 @@
-var a = 1;
-function b() {
-    a = 10;
-    return;
-    function a() {
-      
+// Find the longest word in a string  
+// Input: "The quick brown fox jumped over the lazy dog"
+// Output: "jumped"
+// Input :"The quick brown fox jumps over the lazy dog"
+// Output:["quick","brown","jumps"]
+const str = "The quick brown fox jumped over the lazy dog";
+const longestWords = [];
+
+const arrOfStrings = str.split(" ");
+const lengsOfStrs=arrOfStrings.map((str1)=>str1.length);
+let longestWord=Math.max(lengsOfStrs);;
+arrOfStrings.forEach((singleStr)=>{
+    if(singleStr.length>=longestWord.length){
+        longestWord=singleStr;
+        longestWords.push(singleStr)
     }
-}
-b();
-console.log(a);
+})
+
+console.log(longestWord)
