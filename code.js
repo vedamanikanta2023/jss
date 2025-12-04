d4f45af9324e0e25ad4b80e48e227eb44085a371
vedamanikanta <manikanta.vedam@gmail.com>
2025-12-05 00:54:54 +0530

    type coersion

--- code.js
+++ code.js
@@ -1,18 +1,4 @@
-//----difference between var vs (const and let)---//
+var x = 33
+var y = "3"
 
-function someFun(){
-  if(true){
-    var game = "bedam"
-  }
-  if(true){
-    let name = "vedam"
-  }
-  
-  if(true){
-    const fame = "actor"
-  }
-  console.log(game,name);
-  console.log(fame);
-}
-
-someFun()
+console.log(y-x) 
