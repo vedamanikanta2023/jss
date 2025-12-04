d614efa13bc311e3d2f893cedf6682d09ab078b3
vedamanikanta <manikanta.vedam@gmail.com>
2025-12-04 22:15:27 +0530

    factory function

--- code.js
+++ code.js
@@ -1,36 +1,12 @@
-// Find the longest word in a string
-// Input: "The quick brown fox jumped over the lazy dog"
-// Output: "jumped"
-// Input :"The quick brown fox jumps over the lazy dog"
-// Output:["quick","brown","jumps"]
-
-const inputs = [
-  "The quick brown fox jumped over the lazy dog",
-  "The quick brown fox jumps over the lazy dog",
-];
-
-function getLongestWordInString(str) {
-  const longestWords = [];
-
-  const arrOfStrings = str.split(" ");
-  const lengsOfStrs = arrOfStrings.map((str1) => {
-    return str1.length;
-  });
-
-  let longestWordLength = Math.max(...lengsOfStrs);
-
-  arrOfStrings.forEach((singleStr) => {
-    if (singleStr.length === longestWordLength) {
-      longestWords.push(singleStr);
+function createCar(color, brand){
+  return {
+    color,
+    brand,
+    start:function(){
+      console.log("started",color,this);
     }
-  });
-
-  if (longestWords.length > 1) {
-    console.log(longestWords);
-    return;
   }
-
-  console.log(longestWords[0]);
 }
 
-inputs.forEach((str) => getLongestWordInString(str));
+const car = createCar("red","sierra");
+car.start()
