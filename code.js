3afc8a51f90ff4739dd38a90d902a7aebd0719ee
vedamanikanta <manikanta.vedam@gmail.com>
2025-12-04 23:13:42 +0530

    constructor function

--- code.js
+++ code.js
@@ -1,12 +1,12 @@
-function createCar(color, brand){
-  return {
-    color,
-    brand,
-    start:function(){
-      console.log("started",color,this);
-    }
-  }
+function Car(color, brand) {
+  this.color = color;
+  this.brand = brand;
+  this.start = function() {
+    console.log("started",this.color);
+  };
+  // return this
 }
 
-const car = createCar("red","sierra");
-car.start()
+let car1 = new Car("blue", "Audi");
+car1.start()
+// console.log(car1.start());
