3376580abe40708d0e70e79f15e68a23ce0ff884
vedamanikanta <manikanta.vedam@gmail.com>
2025-12-06 23:18:24 +0530

    changed the logic for non repetative value

--- code.js
+++ code.js
@@ -1,17 +1,27 @@
-let nums = [2,5,3,2,6,5,7,1,2];
+let arrOfItems = [2, 5, 3, 2, 6, 5, 7, 1, 2];
 
-const objNums = {};
+function getFirstNonRepetativeItemOfArray(arrOfItems) {
+  const objNums = {};
 
-nums.forEach(num=>{
-    const objNum = objNums[String(num)]
-    if(objNums[String(num)]){
-        objNums[String(num)]=objNums[String(num)]+1
-    }else{
-        objNums[String(num)] =1
+  arrOfItems.forEach((num) => {
+    if (objNums[String(num)]) {
+      objNums[String(num)] = objNums[String(num)] + 1;
+    } else {
+      objNums[String(num)] = 1;
     }
+  });
 
-});
+  let firstNonRepetitiveNum;
 
-let firstNonRepetitiveNums = nums.filter(num=>objNums[String(num)]===1);
+  for (let singleNum of arrOfItems) {
+    if (objNums[String(singleNum)] === 1) {
+      firstNonRepetitiveNum = singleNum;
+      break;
+    }
+  }
 
-console.log(firstNonRepetitiveNums[0]);
+  console.log("firstNonRepetitiveNum",firstNonRepetitiveNum);
+  return firstNonRepetitiveNum;
+}
+
+getFirstNonRepetativeItemOfArray(arrOfItems)
