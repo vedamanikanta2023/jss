fa1f058b6609ab8bbdf8119ad979e3c07e4e269e
vedamanikanta <manikanta.vedam@gmail.com>
2026-04-21 17:09:35 +0530

    event listeners learnings

---
+++ namaste_javascript/eventListeners.js
@@ -0,0 +1,7 @@
+function addEventListener() {
+  let count = 0;
+  document.getElementById("clickMe").addEventListener("click", function xyz() {
+    console.log("Button Clicked", ++count);
+  });
+}
+addEventListener();
