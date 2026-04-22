3020a4a01e08f910a1b2c9154d8eb7e69e0ca68d
vedamanikanta <manikanta.vedam@gmail.com>
2026-04-23 01:42:37 +0530

    event loop learnings

--- namaste_javascript/eventLoop.js
+++ namaste_javascript/eventLoop.js
@@ -1,6 +1,12 @@
 console.log('start')
 document.getElementById('btn')
-.addEventListener('click',function cb(){
+.addEventListener('click',function cbT(){
     console.log('Callback')
 })
+
+fetch('https://api.netflix.com')
+.then(function cbF(){
+    console.log('CB Netflix');
+})
+.catch(e=>{console.log(e)})
 console.log('start')
