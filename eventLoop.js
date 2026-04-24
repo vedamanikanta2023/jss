102b9a1371f2ad33b78236ba21c7a3180c0196f5
vedamanikanta <manikanta.vedam@gmail.com>
2026-04-24 23:32:23 +0530

    typeof

--- eventLoop.js
+++ eventLoop.js
@@ -1,8 +1,2 @@
-console.log('Start');
-setTimeout(() => {
-    console.log('Timeout 1');
-}, 0);
-Promise.resolve().then(() => {
-    console.log('Promise 1');
-});
-console.log('End');
+var name;
+console.log(typeof name)
