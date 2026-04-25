fd6fcc7304f0011b42d71be38999202642f48775
vedamanikanta <manikanta.vedam@gmail.com>
2026-04-25 22:24:31 +0530

    promises learing

--- eventLoop.js
+++ eventLoop.js
@@ -1,2 +1,23 @@
 var name;
-console.log(typeof name)
+console.log(typeof name);
+var age = null;
+console.log(null !== age);
+
+// promise chaining
+function promising(a, b) {
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      resolve(a + b);
+    });
+  })
+    .then((response) => {
+      return response * 2;
+    })
+    .then((response) => {
+      return response/3
+    });
+}
+promising(4, 5)
+.then(res=>{
+    console.log(res)
+}).catch(e=>console.log(e))
