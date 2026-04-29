1bc2aa36c117e048471370a2e0ff657bfb83e9ef
vedamanikanta <manikanta.vedam@gmail.com>
2026-04-29 06:59:34 +0530

    type coersion

---
+++ typeCoersion.js
@@ -0,0 +1,13 @@
+/**
+ * Type Coersion is the automatic converstion of values from one to another, ex string to number
+ * 
+ *  */ 
+console.log(1+'2')
+console.log(1-'2')
+console.log('8'*2)
+console.log("8"*'2')
+console.log(true*"2")
+console.log(false*"2")
+console.log("22"*true)
+const zero = String(0)
+console.log(zero?'yes':'no')
