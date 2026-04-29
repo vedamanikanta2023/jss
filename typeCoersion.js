f19d3d60f9ed065c711015b3873504d030bfe048
vedamanikanta <manikanta.vedam@gmail.com>
2026-04-29 08:29:57 +0530

    set and check 32 bit

--- typeCoersion.js
+++ typeCoersion.js
@@ -11,4 +11,16 @@ console.log(false*"2")
 console.log("22"*true)
 const zero = String(0)
 console.log(zero?'yes':'no')
-console.log(0===false)
+console.log(0===false,2**320,'square')
+
+const arr = [1,1,2,5,7,0,5]
+const set = new Set(arr)
+console.log([...set],'set')
+
+
+
+const is32Bit = (num)=>{
+    return Number.isInteger(num) && num>-(2**31) && num <=(2**31)-1
+}
+
+console.log(is32Bit(321))
