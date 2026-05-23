749d0ef38819bbba223f1e8267f893773bd27739
vedamanikanta <manikanta.vedam@gmail.com>
2026-05-23 07:29:45 +0530

    promise chaining

--- promises.js
+++ promises.js
@@ -20,20 +20,46 @@ fetData
     console.log("finally");
   });
 
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 900, "Promise 1 result");
+});
 
-  const promise1 = new Promise((resolve,reject)=>{
-    setTimeout(resolve,900,'Promise 1 result')
-})
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 600, "Promise 2 result");
+});
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 200, "Promise 3 result");
+});
 
-const promise2 = new Promise((resolve,reject)=>{
-    setTimeout(resolve,600,'Promise 2 result')
-})
-const promise3 = new Promise((resolve,reject)=>{
-    setTimeout(resolve,200,'Promise 3 result')
-})
+Promise.all([promise1, promise2, promise3])
+  .then((result) => {
+    console.log("result: ", result);
+  })
+  .catch((e) => console.error("error", e));
 
-Promise.all([promise1,promise2,promise3])
-.then(result=>{
-    console.log('result: ',result)
-})
-.catch(e=>console.error('error',e))
+// Promise chaining
+const promise = new Promise((success, failure) => {
+  if (1) {
+    setTimeout(success, 1000,20);
+  } else {
+    failure("failed Promise");
+  }
+});
+
+console.log("promise", promise);
+
+promise
+  .then((reso, one) => {
+    console.log(reso);
+
+    return new Promise((res, rej) => {
+      res(reso * 3);
+    });
+  })
+  .then((result) => {console.log("res", result)
+    return new Promise((res,rej)=>{
+      res(result/2)
+    })
+  })
+  .then(result=>console.log('result after the chaining :', result))
+  .catch((e) => console.log("catched :", e));
