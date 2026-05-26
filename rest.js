49278c0ff354173d70581e771dcc64b4106d1569
vedamanikanta <manikanta.vedam@gmail.com>
2026-05-26 11:48:50 +0530

    rest operatior and arguments of function

---
+++ rest.js
@@ -0,0 +1,11 @@
+function sum(a,b,...numbers){
+    console.log(a)
+    console.log(b)
+    console.log(numbers)
+    return arguments
+}
+
+
+const res = sum(1,2,3,4,5,6,7,8,9)
+
+console.log(res )
