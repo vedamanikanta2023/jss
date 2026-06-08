221783d474c83b73d3306ef50fd1c52ca0b6f975
vedamanikanta <manikanta.vedam@gmail.com>
2026-06-08 12:10:50 +0530

    finally block in generator  block

--- generatorFunction.js
+++ generatorFunction.js
@@ -1,17 +1,33 @@
-function* myGenerator() {
-  yield 1;
-  yield 2;
-  yield 3;
-}
+// function* myGenerator() {
+//   yield 1;
+//   yield 2;
+//   yield 3;
+// }
 
-const gen = myGenerator();
+// const gen = myGenerator();
 // console.log(gen.next(), "gen1");
 // console.log(gen.next(), "gen2");
 // console.log(gen.next(), "gen3");
 
-
 // for (let item of gen){
-    // console.log(item,'item');
+// console.log(item,'item');
 // }
 
-console.log([...gen])
+// console.log([...gen])
+
+function* generatorFunction() {
+    try{
+        yield "step 1";
+        yield "step 2";
+        yield "step 3";
+    }finally{
+        yield ('executed finally block')
+    }
+}
+
+const gen = generatorFunction();
+
+console.log(gen.next());
+console.log(gen.return());
+console.log(gen.next());
+console.log(gen.next());
