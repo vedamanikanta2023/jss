95407695dd0537c0d665821d2ae021fb81ecba45
vedamanikanta <manikanta.vedam@gmail.com>
2026-06-09 20:11:03 +0530

    object learing referencing other values

--- object2.js
+++ object2.js
@@ -11,3 +11,16 @@ const {
   fullName: { first },
 } = user;
 // console.log(first);
+
+let c = {greeting:'Hi good mornign'};
+
+let d ;
+d=JSON.parse(JSON.stringify(c));
+c.greeting = 'Hello'
+console.log(c.greeting,d==c);
+
+let person = {name:"hello"};
+
+const members = [person];
+person.name='null';
+console.log(members);
