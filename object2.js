b0cd6a497febe1351c68b65131d1598d9bcff1d7
vedamanikanta <manikanta.vedam@gmail.com>
2026-06-09 19:55:04 +0530

    object destructring

---
+++ object2.js
@@ -0,0 +1,13 @@
+let user = {
+  name: "vedamanikanta",
+  age: 29,
+  fullName: {
+    first: "vedamanianta",
+    last: "vanga",
+  },
+};
+
+const {
+  fullName: { first },
+} = user;
+// console.log(first);
