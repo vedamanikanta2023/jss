3dd3143366d450afaa77dd231c990e13ae12545b
vedamanikanta <manikanta.vedam@gmail.com>
2026-06-09 19:22:02 +0530

    array and object methods

--- object.js
+++ object.js
@@ -8,3 +8,12 @@ let { gender:gen='jasf', age } = person;
 let a = 'firstName';
 person['country'] = 'India'
 console.log(person)
+
+const k={}
+
+const b={key:'b'};
+const c={key:'c'};
+
+k[b]=234
+k[c]='ojf'
+console.log(k[b],k)
