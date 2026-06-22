b5b6b573aadeb2dbbca42f4b2afa844e7fd2fbf4
vedamanikanta <manikanta.vedam@gmail.com>
2026-06-22 18:46:06 +0530

    var let const

--- index3.js
+++ index3.js
@@ -2,10 +2,27 @@ const data = false;
 
 const data1 = new Boolean(false);
 
-if(data){
-    console.log('first')
+if (data) {
+  console.log("first");
 }
 
-if(data1){
-console.log('second',data1)
+if (data1) {
+  console.log("second", data1);
 }
+
+function aa() {
+  for (let i = 0; i < 3; i++) {
+    setTimeout(() => {
+      console.log(i);
+    }, 1000);
+
+    // function c(i){
+    // setTimeout(() => {
+    //         console.log(i);
+    //       }, 1000);
+    // }
+    // c(i);
+  }
+}
+
+aa();
