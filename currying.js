1eb3840c7cd94e9f3ca6ee6ebb18f30cfc681a0b
vedamanikanta <manikanta.vedam@gmail.com>
2026-06-24 23:56:47 +0530

    learning currying

--- currying.js
+++ currying.js
@@ -1,6 +1,6 @@
+//implement 
 
 function add(a){
-
     return function _(b){
         if(b) return add(a+b);
         return a;
@@ -8,3 +8,24 @@ function add(a){
 }
 
 console.log(add(5)(2)(4)(8)());
+
+
+//implement this code//
+const calc = {
+    total:0,
+    add(a){
+        this.total+=a
+        return this
+    },
+    multiply(b){
+        this.total*=b
+        return this
+    },
+    subtract(c){
+        this.total-=c
+        return this
+    }
+}
+
+const result = calc.add(10).multiply(5).subtract(30).add(15);
+console.log(result.total,'result this')
