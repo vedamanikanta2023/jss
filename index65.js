15a7dccd07cb6740eca958c9d7826b9cd8569887
vedamanikanta <manikanta.vedam@gmail.com>
2026-07-05 15:35:37 +0530

    pracciceing js

--- index65.js
+++ index65.js
@@ -1,8 +1,18 @@
-if (true) {
-  var name = "mani";
-  let name2 = "no";
-  var name = "nokfads";
-  name2 = "mokfds";
-}
+// console.log(124=='124',10 == '10abc',NaN===NaN,true==20);
 
-console.log(name, "varing -->");
+// if(20){
+//     console.log('20 is true')
+// }
+
+// queueMicrotask(()=>console.log('queuedMicrotask'))
+
+const promise1 = Promise.resolve("Success!");
+const promise2 = Promise.reject("Error occurred");
+const promise3 = new Promise((resolve) => setTimeout(resolve, 100, "Delayed success"));
+
+Promise.all([promise1, promise2, promise3])
+  .then((results) => {
+    console.log(results);
+  }).catch(e=>{
+    console.log(e)
+  });
