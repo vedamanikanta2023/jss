bd606aff95affbd72e886586214799f3491f2149
vedamanikanta <manikanta.vedam@gmail.com>
2026-07-17 23:55:25 +0530

    fetching data learing

--- fileReading.js
+++ fileReading.js
@@ -4,13 +4,14 @@ const fs = require('fs');
 const path = `c:/Users/ADMIN/Downloads/VedamanikantamVangReact_Developer.pdf`;
 const data = fs.readFileSync(path, 'utf8');
 console.log(data); // Blocks until file is read
-console.log('End of the program');}
+console.log('End of the program');
+}
 
 console.log('Start of the program');
 
-fetch('https://api.example.com/data')
+fetch('https://dummyjson.com/products')
   .then((response) => response.json())
   .then((data) => console.log(data)) // Non-blocking
-  .catch((error) => console.error(error));
+  .catch((error) => console.error('er',error));
 
 console.log('End of program');
