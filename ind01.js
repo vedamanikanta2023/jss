b939af4e64d23811c84f07a61240f9479b6733e2
vedamanikanta <manikanta.vedam@gmail.com>
2026-07-17 23:59:33 +0530

    akjsdffs

--- ind01.js
+++ ind01.js
@@ -1,9 +1,19 @@
 
+function fetching(){
+
+    // fetch('https://dummyjson.com/products')
+    //   .then((response) => response.json())
+    //   .then((data) => console.log(data)) // Non-blocking
+    //   .catch((error) => console.error('er',error));
+
+      return new Promise((res,rej)=>{
+        fetch('https://dummyjson.com/products')
+        .then(response=>response.json())
+        .then(data=>res(data));
+      })
+}
+
 console.log('Start of the program');
 
-fetch('https://dummyjson.com/products')
-  .then((response) => response.json())
-  .then((data) => console.log(data)) // Non-blocking
-  .catch((error) => console.error('er',error));
-
+const fetchedDAta = fetching()
 console.log('End of program');
