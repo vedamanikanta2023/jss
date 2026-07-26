c8aba0c9cdc4ee2271e9638b9cab5f4c16021b76
vedamanikanta <manikanta.vedam@gmail.com>
2026-07-26 23:43:31 +0530

    sample 3

--- AdvancedJavascript/index32809.js
+++ AdvancedJavascript/index32809.js
@@ -1,6 +1,6 @@
 const array1 = [1, 2, 3];
 const array2 = [4, 5, 6];
 
-const mergedArray = [...array1, ...array2];
+const mergedArray = array1.concat(array2);
 
 console.log(mergedArray); // Output: [1, 2, 3, 4, 5, 6]
