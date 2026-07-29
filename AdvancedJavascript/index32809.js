917087c1be0278c4b441e8d8400c94629e78bc48
vedamanikanta <manikanta.vedam@gmail.com>
2026-07-30 01:30:22 +0530

    advanced javasccript : libuv

--- AdvancedJavascript/index32809.js
+++ AdvancedJavascript/index32809.js
@@ -1,6 +1,21 @@
-// process // argv
-const argn = process.argv[2]
-const nn = process.argv[3]
-console.log(argn,nn)
+// // process // argv
+// const argn = process.argv[2]
+// const nn = process.argv[3]
+// console.log(argn,nn)
 
-console.log(process.argv,'argv');
+// console.log(process.argv,'argv');
+
+
+setTimeout(() => {
+    console.log('1');
+}, 0);
+setTimeout(()=>{
+    console.log('2');
+},0)
+setImmediate(()=>{
+    console.log('3');
+})
+setImmediate(()=>{
+    console.log('4');
+})
+// console.log('start');
