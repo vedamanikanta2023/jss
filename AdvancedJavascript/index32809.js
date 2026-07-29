d5799f10822f72e118dc9709d8972c907d23e239
vedamanikanta <manikanta.vedam@gmail.com>
2026-07-29 23:53:07 +0530

    scope of  variable types learning

--- AdvancedJavascript/index32809.js
+++ AdvancedJavascript/index32809.js
@@ -4,3 +4,13 @@ const nn = process.argv[3]
 console.log(argn,nn)
 
 console.log(process.argv,'argv');
+
+if (true) {
+  var foo = 1;
+  let bar = 2;
+  const baz = 3;
+}
+
+console.log(foo); // 1
+console.log(bar); // ReferenceError
+console.log(baz); // ReferenceError
