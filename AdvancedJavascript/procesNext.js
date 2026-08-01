9921fd0df0dd7fe6a25faf945743f06a761cd8f7
vedamanikanta <manikanta.vedam@gmail.com>
2026-08-01 23:54:19 +0530

    advanced js: order of execution

--- AdvancedJavascript/procesNext.js
+++ AdvancedJavascript/procesNext.js
@@ -2,9 +2,19 @@ process.nextTick(function tick(){
     console.log('nextTick');
     // process.nextTick(tick)
 })
+process.nextTick(function tick(){
+    console.log('2nd nextTick');
+    // process.nextTick(tick)
+})
 Promise.resolve().then(()=>{
     console.log('promise');
 })
 setTimeout(function(){
     console.log('timeout');
 })
+
+Promise.resolve().then(()=>{
+    console.log('promise resolved');
+});
+
+console.log('sampleing');
