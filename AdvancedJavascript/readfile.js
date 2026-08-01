558cde9aa50b13e6f25ec537acb85bdc5217d72c
vedamanikanta <manikanta.vedam@gmail.com>
2026-08-02 00:00:38 +0530

    advance js : reading file

--- AdvancedJavascript/readfile.js
+++ AdvancedJavascript/readfile.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 
-fs.readFile(__filename,()=>{
+fs.readFile(__filename,(err)=>{
+    console.log('file reading..!',err);
     setTimeout(()=>console.log('jsdfa'))
         setTimeout(()=>console.log('32r90pek'))
 
 })
+
+console.log('file reading');
