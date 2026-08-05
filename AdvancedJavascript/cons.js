5bcb8abe77290ba03a6acf332ac4e21f42acca9e
vedamanikanta <manikanta.vedam@gmail.com>
2026-08-06 00:59:35 +0530

    index.js

---
+++ AdvancedJavascript/cons.js
@@ -0,0 +1,8 @@
+const {add,sub} = require('./math1')
+
+console.log(
+
+    // add,
+    // require.cache
+    add(2,3),sub(6,1)
+);
---
+++ AdvancedJavascript/math1/index.js
@@ -0,0 +1,2 @@
+export {default as  add } from './add.js';
+export {default as sub } from './sub.js';
