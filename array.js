4215d3996866ee35e4a185fe5a667b322fccd638
vedamanikanta <manikanta.vedam@gmail.com>
2026-08-10 23:26:41 +0530

    ekmpfpmdsf

--- array.js
+++ array.js
@@ -1,9 +1,9 @@
 let myArray = [5, "six", 2, 8.2];
-// myArray[1] = 'mani'
-// let nooo = myArray.push('joasdf')
-// let removedItem = myArray.pop(0)
+myArray[1] = 'mani'
+let nooo = myArray.push('joasdf')
+let removedItem = myArray.pop(0)
 
-// console.log(myArray,'myArray',removedItem)
+console.log(myArray,'myArray',removedItem)
 
 for (let item of myArray){
     if(item===2) break;
