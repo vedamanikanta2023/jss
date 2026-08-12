cdda27d6c11f10501f22a66450f5d50324fb884d
vedamanikanta <manikanta.vedam@gmail.com>
2026-08-13 00:00:11 +0530

    asdfasjdflasd

---
+++ genAI/asdfj.js
@@ -0,0 +1,413 @@
+Extracted Statement Details:
+ {
+  "accountHolder": "Mr. VANGA VEDAMANIKANTA",
+  "accountNumber": "XXXXXXX3366",
+  "bankName": "SBI",
+  "recentTransactions": [
+    {
+      "amount": 2000,
+      "date": "01-07-26",
+      "description": "UPI/CR/724188945010/THIRUMAL/SBIN/9951770825/Payme",
+      "type": "CREDIT"
+    },
+    {
+      "amount": 40,
+      "date": "01-07-26",
+      "description": "UPI/DR/618256805840/CHANDRA /SBIN/9533955083/UPI",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 100,
+      "date": "01-07-26",
+      "description": "UPI/DR/654803778099/B PRIYANKA/FDRL/BHARATPE.9/Pay",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 30,
+      "date": "01-07-26",
+      "description": "UPI/DR/618205325415/CHANDRA /SBIN/9533955083/UPI",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 627.76,
+      "date": "03-07-26",
+      "description": "UPI/DR/655028131534/RELIANCE/CITI/jio@citiba/JIO20",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 10,
+      "date": "04-07-26",
+      "description": "UPI/DR/655130287017/Mr VENK/YESB/q592000588/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 455,
+      "date": "04-07-26",
+      "description": "UPI/DR/609727553565/BBNOW/HDFC/bbnow.ebz@/Pay",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 32,
+      "date": "04-07-26",
+      "description": "UPI/DR/655107322491/GADDAM /YESB/Q500185858/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 30,
+      "date": "04-07-26",
+      "description": "UPI/DR/655117360271/SRI SAI /YESB/q775798936/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 100,
+      "date": "05-07-26",
+      "description": "UPI/CR/269741665385/CHANDRAS/SBIN/vanga.chan/Payme",
+      "type": "CREDIT"
+    },
+    {
+      "amount": 165,
+      "date": "05-07-26",
+      "description": "UPI/DR/655203449286/Blinkit/YESB/paytm-blin/Blinki",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 40,
+      "date": "06-07-26",
+      "description": "UPI/DR/655318655827/GOLLA NA/UNBA/BHARATPE.9/Pay t",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 50,
+      "date": "06-07-26",
+      "description": "UPI/DR/618715164897/CHANDRA /SBIN/9533955083/UPI",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 2150,
+      "date": "07-07-26",
+      "description": "UPI/DR/737643431075/KODURI S/SBIN/6301706163/Payme",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 595,
+      "date": "07-07-26",
+      "description": "UPI/DR/655431806571/CRED Club/UTIB/cred.club@/paym",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 400,
+      "date": "07-07-26",
+      "description": "UPI/CR/322340536603/TANNEERU/NESF/8686302198/Payme",
+      "type": "CREDIT"
+    },
+    {
+      "amount": 30,
+      "date": "07-07-26",
+      "description": "UPI/DR/655411818494/Machukur/YESB/q510061358/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 100,
+      "date": "08-07-26",
+      "description": "UPI/DR/655511945445/NEW PUMP/YESB/q006813008/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 21,
+      "date": "09-07-26",
+      "description": "ACHCr HDFC05717000027455 TMPVL DIV 09072026",
+      "type": "CREDIT"
+    },
+    {
+      "amount": 10,
+      "date": "09-07-26",
+      "description": "UPI/DR/210389155956/KONDA MA/YESB/paytm.s20x/Sent",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 2300,
+      "date": "09-07-26",
+      "description": "UPI/DR/610032211582/CHANDRA /SBIN/vanga.chan/Sent",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 120,
+      "date": "10-07-26",
+      "description": "UPI/CR/014654796259/MACHA P/SBIN/9642675757/Payme",
+      "type": "CREDIT"
+    },
+    {
+      "amount": 120,
+      "date": "10-07-26",
+      "description": "UPI/DR/655725210809/VADTHYA /YESB/Q655672457/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 30,
+      "date": "11-07-26",
+      "description": "UPI/DR/655813324948/SHARADA /YESB/paytmqr6yh/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 60,
+      "date": "11-07-26",
+      "description": "UPI/DR/655807361317/Sunil Ch/FDRL/bharatpe.9/Pay t",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 10,
+      "date": "11-07-26",
+      "description": "UPI/DR/655819409008/UDTHAWAR/YESB/paytm.s22s/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 14,
+      "date": "11-07-26",
+      "description": "UPI/DR/655810443417/PRADAHAN/YESB/paytm.s1ef/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 120,
+      "date": "12-07-26",
+      "description": "UPI/DR/619312789626/CHANDRA /SBIN/9533955083/UPI",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 120,
+      "date": "12-07-26",
+      "description": "UPI/DR/655911581243/BALASANI/YESB/Q677882584/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 210,
+      "date": "12-07-26",
+      "description": "UPI/DR/655913568425/ABR CAFE/YESB/abrcafeand/Payme",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 150,
+      "date": "14-07-26",
+      "description": "UPI/DR/656104825479/Kandula /YESB/paytm.s241/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 10,
+      "date": "14-07-26",
+      "description": "UPI/DR/656102824710/KONDA MA/YESB/paytm.s20x/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 40,
+      "date": "15-07-26",
+      "description": "UPI/DR/656200958084/BALA RAJ/YESB/q094821426/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 30,
+      "date": "18-07-26",
+      "description": "UPI/DR/656531370046/SRI SAI /YESB/q775798936/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 10,
+      "date": "19-07-26",
+      "description": "UPI/DR/656624418377/DEVAGONI/YESB/paytmqr6i1/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 150,
+      "date": "19-07-26",
+      "description": "UPI/DR/620026688185/CHANDRA /SBIN/9533955083/UPI",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 660,
+      "date": "19-07-26",
+      "description": "UPI/CR/960702724482/POTLAPAL/SBIN/prasadpjd1/Payme",
+      "type": "CREDIT"
+    },
+    {
+      "amount": 40,
+      "date": "19-07-26",
+      "description": "UPI/DR/656628522093/Karre Na/YESB/paytmqr6jt/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 40,
+      "date": "19-07-26",
+      "description": "UPI/DR/656624545261/Karre Na/YESB/paytmqr6jt/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 209.5,
+      "date": "21-07-26",
+      "description": "UPI/DR/656815735750/BIGBASKET/KKBK/bigbasket./Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 32,
+      "date": "21-07-26",
+      "description": "UPI/DR/656801787775/UDTHAWAR/YESB/paytm.s27y/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 1100,
+      "date": "22-07-26",
+      "description": "UPI/DR/656915864909/ANGEL ON/ICIC/angelone.n/Payin",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 15,
+      "date": "22-07-26",
+      "description": "UPI/DR/656925908651/Machukur/YESB/q510061358/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 10,
+      "date": "22-07-26",
+      "description": "UPI/DR/656917920229/UDTHAWAR/YESB/paytm.s27y/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 100,
+      "date": "23-07-26",
+      "description": "UPI/DR/620485804600/CHANDRA /SBIN/9533955083/UPI",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 15,
+      "date": "24-07-26",
+      "description": "UPI/DR/657113167535/Machukur/YESB/Q559678072/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 10,
+      "date": "24-07-26",
+      "description": "UPI/DR/657100166002/KONDA MA/YESB/paytm.s20x/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 5,
+      "date": "24-07-26",
+      "description": "UPI/DR/657126175324/KONDA MA/YESB/paytm.s20x/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 10,
+      "date": "24-07-26",
+      "description": "UPI/DR/657124194114/ALLURI V/YESB/paytmqr6hx/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 10,
+      "date": "25-07-26",
+      "description": "UPI/DR/657203326714/UDTHAWAR/YESB/paytm.s27y/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 65,
+      "date": "26-07-26",
+      "description": "UPI/DR/620735193780/CHANDRA /SBIN/9533955083/UPI",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 32,
+      "date": "26-07-26",
+      "description": "UPI/DR/657324467787/UDTHAWAR/YESB/paytm.s22s/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 72,
+      "date": "27-07-26",
+      "description": "ACHCr DEUT00003000002043 684258",
+      "type": "CREDIT"
+    },
+    {
+      "amount": 10,
+      "date": "27-07-26",
+      "description": "UPI/DR/657428572695/PITANI N/YESB/paytm.s263/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 10,
+      "date": "27-07-26",
+      "description": "UPI/DR/657400595733/UDTHAWAR/YESB/paytm.s22s/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 25,
+      "date": "28-07-26",
+      "description": "UPI/DR/657525729004/CHANDRA /SBIN/9533955083/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 10,
+      "date": "28-07-26",
+      "description": "UPI/DR/657501739926/UDTHAWAR/YESB/paytm.s22s/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 15,
+      "date": "29-07-26",
+      "description": "UPI/DR/657603853979/Kadari R/YESB/paytm.s1o5/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 10,
+      "date": "29-07-26",
+      "description": "UPI/DR/657620874688/UDTHAWAR/YESB/paytm.s27y/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 10,
+      "date": "30-07-26",
+      "description": "UPI/DR/621113281222/CHANDRA /SBIN/9533955083/UPI",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 5000,
+      "date": "30-07-26",
+      "description": "UPI/DR/290518206096/POTLAPAL/SBIN/prasadpjd@/Payme",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 51,
+      "date": "31-07-26",
+      "description": "ACHCr NACH00000000020087 CDSL Fnl 2025 26",
+      "type": "CREDIT"
+    },
+    {
+      "amount": 36,
+      "date": "31-07-26",
+      "description": "ACHCr SBIN00882000011904 TCS1stIntDiv31072026",
+      "type": "CREDIT"
+    },
+    {
+      "amount": 30,
+      "date": "31-07-26",
+      "description": "UPI/DR/657808088990/UDTHAWAR/YESB/paytm.s22s/Paid",
+      "type": "DEBIT"
+    },
+    {
+      "amount": 10,
+      "date": "31-07-26",
+      "description": "UPI/DR/621286165614/CHANDRA /SBIN/9533955083/UPI",
+      "type": "DEBIT"
+    },
+    {
+      "date": "31-07-26",
+      "description": "UPI/DR/311506251420/Rajarath/YESB/q524194254/Sent",
+      "type": "DEBIT"
+    }
+  ],
+  "statementPeriod": {
+    "endDate": "31-07-26",
+    "startDate": "01-07-26"
+  },
+  "summary": {
+    "closingBalance": 30902.73,
+    "openingBalance": 42144.99,
+    "totalDeposits": 3460,
+    "totalWithdrawals": 14702.26
+  }
+}
